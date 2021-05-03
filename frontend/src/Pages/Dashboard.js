d1752db9887bd13508c42c2b8476cc41a7863b56
JurievdS <jurievanderschyff@gmail.com>
2021-05-03 19:58:43 +0200

    Continued rework

--- frontend/src/Pages/Dashboard.js
+++ frontend/src/Pages/Dashboard.js
@@ -1,14 +1,16 @@
-import React, { useState, useEffect, Fragment } from "react";
+import React, { useState, useEffect, Fragment, Component } from "react";
 import { useDispatch, useSelector } from "react-redux";
 
-const Dashboard = () => {
-  return (
-    <>
-      <div>
-        <H1>Dashboard</H1>
-      </div>
-    </>
-  );
+class Dashboard extends Component {
+  render(){
+    return (
+        <>
+          <div>
+            <H1>Dashboard</H1>
+          </div>
+        </>
+      );
+  }
 };
 
 export default Dashboard;
