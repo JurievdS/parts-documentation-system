680b7cef54cd5e163db789f6e11897e816fedca9
JurievdS <jurievanderschyff@gmail.com>
2021-05-03 16:00:44 +0200

    Continued rework

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,13 @@
-import React from 'react'
-import { BrowserRouter as Router, Route } from 'react-router-dom'
+import React from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import Dashboard from "./Pages/Dashboard";
 
 function App() {
   return (
     <Router>
-      <Route path="/Dashboard" component={Dashboard}/>
-    
-
+      <main>
+        <Route path="/Dashboard" component={Dashboard} />
+      </main>
     </Router>
   );
 }
---
+++ frontend/src/Pages/Dashboard.js
@@ -0,0 +1,14 @@
+import React, { useState, useEffect, Fragment } from "react";
+import { useDispatch, useSelector } from "react-redux";
+
+const Dashboard = () => {
+  return (
+    <>
+      <div>
+        <H1>Dashboard</H1>
+      </div>
+    </>
+  );
+};
+
+export default Dashboard;
