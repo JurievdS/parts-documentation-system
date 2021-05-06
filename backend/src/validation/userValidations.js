b3108d9b370ae4c34c0497cada912e8b44d37dfe
JurievdS <jurievanderschyff@gmail.com>
2021-05-06 20:14:50 +0200

    Add email validation

--- backend/src/validation/userValidations.js
+++ backend/src/validation/userValidations.js
@@ -17,13 +17,13 @@ function validateNewUser(data) {
     errors.username = "username field is required";
   }
 
-  // if(validator.isEmpty(data.email)){
-  //     errors.email = "Email field is required";
-  // }
+  if(validator.isEmpty(data.email)){
+      errors.email = "Email field is required";
+  }
 
-  // if (!validator.isEmail(data.email)) {
-  //   errors.email = "Email is invalid";
-  // }
+  if (!validator.isEmail(data.email)) {
+    errors.email = "Email is invalid";
+  }
 
   if (!validator.isLength(data.password, { min: 6, max: 30 })) {
     errors.password = "Password must be at least 6 characters long";
